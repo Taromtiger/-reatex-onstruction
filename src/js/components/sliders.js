1e0582ffac08a61dc4f72d413b93624884e236b4
Tarstar <taromtiger@gmail.com>
2022-02-13 18:21:51 +0200

    додано сторінку INTERIOR DESIGN блок 1 і 2

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -3,60 +3,63 @@ const portSlider = document.querySelector('.portfolio-section__items');
 const bodyStyles = window.getComputedStyle(document.body);
 const gap = parseInt(bodyStyles.getPropertyValue('--grid-gap'));
 
-const portfolioSlider = new Swiper(portSlider, {
-  slidesPerView: 3,
-  spaceBetween: gap,
-  on: {
-    init: function () {
-      console.log('swiper initialized');
+if (portSlider) {
 
-      const activeSlide = portSlider.querySelector('.swiper-slide-active');
-      
-      const nextActiveSlide = activeSlide.nextElementSibling;
-      
-      const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
-
+  const portfolioSlider = new Swiper(portSlider, {
+    slidesPerView: 3,
+    spaceBetween: gap,
+    on: {
+      init: function () {
+        console.log('swiper initialized');
+  
+        const activeSlide = portSlider.querySelector('.swiper-slide-active');
+        
+        const nextActiveSlide = activeSlide.nextElementSibling;
+        
+        const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+  
+        activeSlide.classList.add('slider-visible');
+        nextActiveSlide.classList.add('slider-visible');
+        nextNextActiveSlide.classList.add('slider-visible');
+      },
+    },
+    navigation: {
+      nextEl: '.portfolio-section__next',
+      prevEl: '.portfolio-section__prev',
+    },
+  });
+  
+  document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
+    const activeSlide = portSlider.querySelector('.swiper-slide-next');
+  
+    document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    });
+        
+    if (activeSlide.previousElementSibling) {
+      const nextActiveSlide = activeSlide.previousElementSibling;
       activeSlide.classList.add('slider-visible');
       nextActiveSlide.classList.add('slider-visible');
-      nextNextActiveSlide.classList.add('slider-visible');
-    },
-  },
-  navigation: {
-    nextEl: '.portfolio-section__next',
-    prevEl: '.portfolio-section__prev',
-  },
-});
-
-document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
-  const activeSlide = portSlider.querySelector('.swiper-slide-next');
-
-  document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
-    el.classList.remove('slider-visible');
+      activeSlide.nextElementSibling.classList.add('slider-visible');
+    }
   });
-      
-  if (activeSlide.previousElementSibling) {
-    const nextActiveSlide = activeSlide.previousElementSibling;
+  
+  document.querySelector('.portfolio-section__').addEventListener('click', () => {
+    const activeSlide = portSlider.querySelector('.swiper-slide-active');
+        
+    const nextActiveSlide = activeSlide.nextElementSibling;
+    
+    const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+  
+    document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    });
+  
     activeSlide.classList.add('slider-visible');
     nextActiveSlide.classList.add('slider-visible');
-    activeSlide.nextElementSibling.classList.add('slider-visible');
-  }
-});
-
-document.querySelector('.portfolio-section__').addEventListener('click', () => {
-  const activeSlide = portSlider.querySelector('.swiper-slide-active');
-      
-  const nextActiveSlide = activeSlide.nextElementSibling;
-  
-  const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
-
-  document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
-    el.classList.remove('slider-visible');
+    nextNextActiveSlide.classList.add('slider-visible');
   });
-
-  activeSlide.classList.add('slider-visible');
-  nextActiveSlide.classList.add('slider-visible');
-  nextNextActiveSlide.classList.add('slider-visible');
-});
+}
 
 const testimoniasSlider = new Swiper('.testimonias__items', {
   slidesPerView: 1,
